216e0637b403723bc293e2bbfa7ad3e6b2365f79
jackyzy823 <jackyzy823@gmail.com>
2022-05-09 14:22:04 +0800

    bump version to v0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = ( here / 'README.md' ).read_text(encoding= 'utf-8')
 
 setup(
 	name='fofaclient',
-	version='0.1.1',
+	version='0.1.2',
 	description="An unoffical fofa client",
 	long_description=long_description,
 	long_description_content_type = "text/markdown",
